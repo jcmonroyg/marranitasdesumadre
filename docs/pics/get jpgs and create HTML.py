7bd7cdae57cb1809b6735905f46842d1310e09b8
jcmonroyg <jcmonroyg@unal.edu.co>
2020-07-14 02:26:10 -0500

    Push from nefertari

--- docs/pics/get jpgs and create HTML.py
+++ docs/pics/get jpgs and create HTML.py
@@ -9,7 +9,7 @@ Created on Wed Jun 24 22:42:06 2020
 from os import listdir
 from os import remove
 
-a = listdir()
+folders = listdir()
 try:
     remove("HTML raw")
 except:
@@ -18,15 +18,33 @@ except:
 html = open("HTML raw", "w+")
 
 numbs = 0
-
-for i in a:
-    if i[-3:] == "jpg":
-        html.write( '<img src="pics/'  )        
-        html.write( i )
-        html.write( '" style=" width:300px ; padding:30px;" > ' )
-        html.write( '\n' )
-        numbs=numbs+1
-        print(numbs)      
+for folder in folders:
+    print(folder)
+    try:
+        a = listdir(folder) 
+        html.write( '<div ' )
+        html.write( 'id="' )
+        html.write( folder[:3] )
+        html.write( '" style="background-color: #E2768A">' )                    
+        html.write( '<h1 style="text-align:center;color:white;">')
+        html.write( folder )
+        html.write( '</h1> </div> ' )
+                
+        for i in a:
+            if i[-3:] == "jpg":
+                html.write( '<img src="pics/' )
+                html.write( folder )
+                html.write( '/' )
+                html.write( i )
+                html.write( '" loading="lazy" style=" width:300px ; padding:10px;" > ' )
+                html.write( '\n' )
+                numbs=numbs+1
+                print(numbs)
+    except:
+        print("not")
+    
+    
+    
 html.close()
 print(numbs)
     
